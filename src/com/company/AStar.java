8001154908b4de9ea28fa3e1608fa0d27f6bd649
Nirmal Nepal <rockink_nirmal@live.com>
2017-02-28 14:30:05 -0500

    Astar alogirthm along with the path saved!!
    Path has been defined.. The previous implementation just had heuristic. So, now ,the decision would be made based on the f = g + h, which how A* has been defined!

--- src/com/company/AStar.java
+++ src/com/company/AStar.java
@@ -7,12 +7,14 @@ public class AStar {
     private final Queue<Edge>[] nodes;
 
     HashMap<Integer, Integer> edgeTo;
+    HashMap<String, Integer> pathCostMap; //from source to target
     List<Integer> list;
     boolean notGoal[];
 
 
     public AStar(Queue<Edge>[] nodes) {
         this.nodes = nodes;
+        this.pathCostMap = new HashMap<String, Integer>();
     }
 
 
@@ -21,29 +23,67 @@ public class AStar {
         return test++;
     }
 
+    
+    //given a stack of path, iteratate over each elem to get the cost
+    //say stack is 1, 2, 3, cost would be + (1, 2) (2, 3)
+    private int pathCostFrom(Stack<Integer> path){
+        return 1;
+    }
+    
 
-    public boolean astar(int current, int goal, Stack<Integer> outcomes){
-
+    Stack<Integer> stack;
+    int cost = Integer.MAX_VALUE;
+    
+    Stack<Integer> costStack; 
+    
+    
+    public boolean astar(int current, int goal){
+        
         int childrenNum = nodes[current].size();
-
-        if (childrenNum == 0 && goal != current) return false;
-
-        System.out.println("processing " + current + "   ");
-
+        if (childrenNum == 0 && goal != current) {
+            
+            return false;
+            
+        }
         if(current == goal) return true;
 
-        Map<Integer, Edge> heuristicByEdges = new TreeMap<>();
+        System.out.println("processing " + current + "   ");
+        
+        costStack.add(current);
+        
 
+        //at this moment, pathCostChildren is 0. it is a g function for heuristic function
+        int pathCostChildren = 0;
+        Map<Integer, Edge> FGHEdges = new TreeMap<>();
         for (Edge e : nodes[current]){ //for every children
-            heuristicByEdges.put(heuristicFn(current, goal), e);
+            costStack.add(e.target);        
+            FGHEdges.put(heuristicFn(e.target, goal) + pathCostFrom(costStack), e);
+            costStack.pop();
         }
-
 
         outcomes.add(current);
 
-        for (Map.Entry<Integer, Edge> entry : heuristicByEdges.entrySet()){
+        //this will automatically start from the lowest one
+        for (Map.Entry<Integer, Edge> entry : FGHEdges.entrySet()){
+                    
+            //from the lowest of f, we would select that one
             if(astar(entry.getValue().target, goal, outcomes)){
-                outcomes.add(entry.getValue().target);
+                Integer target = entry.getValue().target;
+                costStack.add(target); //this is a goal state
+
+                //get the cost till here
+                pathCostChildren = pathCostChildren + entry.getValue().dist;
+                
+                //but check if the cost of this one is less than the previously held one.
+                if(pathCostChildren < cost){
+                    outCome = new Stack();
+                    for (Integer node :  costStack){
+                        outCome.add(node);   
+                    }
+                    cost = pathCostChildren;
+                }
+                
+                costStack.pop();
                 System.out.println(current +"  " + entry.getValue().target);
             }
         }
@@ -51,6 +91,4 @@ public class AStar {
         return false;
 
     }
-
-
 }
